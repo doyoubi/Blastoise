6c3ce1a327cff61026846bc15199925923e1c521
doyoubi <doyoubihgx@gmail.com>
2015-11-25 03:21:31 +0000

    add test bash script

--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
-[ -d build ] || premake gmake
-cd build
+set -e
+
+. ./build_settings.sh
+export PROJ_DIR=${PWD}
+
+if [ ! -d build ]; then
+    git submodule update --init --recursive
+    export GTEST_DIR="${PWD}/gtest/googletest"
+    cd ${GTEST_DIR} && mkdir build && cd build && cmake ${GTEST_DIR} && make
+    cd ${PROJ_DIR} && mkdir build && cd build && mkdir gtest
+    mv ${GTEST_DIR}/build/*.a gtest/
+    cd ${PROJ_DIR} && premake4 gmake
+    rm -rf "${GTEST_DIR}/build"
+fi
+
+cd "${PROJ_DIR}/build"
 make test && ./test/test_program
 
