9260f95dc588de226c3f7e733f7bdce605fadb1e
doyoubi <doyoubihgx@gmail.com>
2016-02-07 10:02:50 +0000

    added tests. finished condition expression parse

--- src/test/parser/test_condition.rs
+++ src/test/parser/test_condition.rs
@@ -160,8 +160,8 @@ fn test_parse_binary() {
 
 #[test]
 fn test_parse_complex_arith_exp() {
-    let tokens = gen_token!("1 + attr_name* table_name.attr_name - (2 + 3)");
-    assert_eq!(tokens.len(), 13);
+    let tokens = gen_token!("1 + attr_name* table_name.attr_name - (2 + +3)");
+    assert_eq!(tokens.len(), 14);
     let mut it = tokens.iter();
     let exp = ArithExpr::parse(&mut it);
     assert_pattern!(exp, Ok(..));
@@ -260,4 +260,22 @@ fn test_binary_cond_exp() {
     test_or_and(ConditionExpr::parse_or);
     test_or_and_with_bracket(ConditionExpr::parse_or);
     test_invalid_tokens(ConditionExpr::parse_or, "1 + 2", 3, CompileErrorType::ParserNoMoreToken)
+}
+
+#[test]
+fn test_complex_cond_expr() {
+    let tokens = gen_token!(
+        "person.money is not null and 1/2 > 3 or 4%(5-6) > 7 and not employee = \"doyoubi\"");
+    assert_eq!(tokens.len(), 26);
+    let mut it = tokens.iter();
+    let exp = ConditionExpr::parse(&mut it);
+    assert_pattern!(exp, Ok(..));
+    let exp = exp.unwrap();
+    let part1 = "((person.money) is not Null(null))";
+    let part2 = "((Integer(1) / Integer(2)) > Integer(3))";
+    let part3 = "((Integer(4) % (Integer(5) - Integer(6))) > Integer(7))";
+    let part4 = "(not (employee = String(doyoubi)))";
+    let result = format!("(({} and {}) or ({} and {}))", part1, part2, part3, part4);
+    assert_eq!(exp.to_string(), result);
+    assert_pattern!(it.next(), None);
 }
