9fc4c76208e7bff94148f10da80581bd2c16c0bd
doyoubi <doyoubihgx@gmail.com>
2016-02-17 12:27:06 +0000

    added test for lru cache callback

--- src/test/store/test_lru.rs
+++ src/test/store/test_lru.rs
@@ -1,27 +1,45 @@
+use std::rc::Rc;
+use std::cell::Cell;
 use ::store::lru::{CacheValue, LruCache};
 
 
+type CountRef = Rc<Cell<u32>>;
+fn gen_count(n : u32) -> CountRef {
+    Rc::new(Cell::new(n))
+}
+
 #[derive(Debug)]
 struct MockValue {
-    pub popcb_called_times : u32,
-    pub poppablecb_called_times : u32,
+    pub pop_callback_called_times : CountRef,
+    pub poppable_called_times : CountRef,
     pub poppable : bool,
     pub key : u64,
 }
 
-fn incr(n : &mut u32) { *n = *n + 1; }
-
 impl CacheValue for MockValue {
     type KeyType = u64;
-    fn pop_callback(&mut self) { incr(&mut self.popcb_called_times) }
-    fn poppable(&mut self) -> bool { incr(&mut self.poppablecb_called_times); self.poppable }
+    fn pop_callback(&mut self) {
+        self.pop_callback_called_times.set((*self.pop_callback_called_times).get() + 1);
+    }
+    fn poppable(&mut self) -> bool {
+        self.poppable_called_times.set((*self.poppable_called_times).get() + 1);
+        self.poppable
+    }
 }
 
 impl MockValue {
     fn new(k : u64) -> Self {
         MockValue{
-            popcb_called_times : 0,
-            poppablecb_called_times : 0,
+            pop_callback_called_times : gen_count(0),  // not used
+            poppable_called_times : gen_count(0),  // not used
+            poppable : true,
+            key : k,
+        }
+    }
+    fn new_with_pop_count(k : u64, poppable : CountRef, pop_callback : CountRef) -> Self {
+        MockValue{
+            pop_callback_called_times : pop_callback,
+            poppable_called_times : poppable,
             poppable : true,
             key : k,
         }
@@ -122,7 +140,6 @@ fn test_put_with_duplicate_key() {
 }
 
 #[test]
-
 fn test_get() {
     {
         let mut c = LruCache::new(1);
@@ -134,7 +151,7 @@ fn test_get() {
         assert_pattern!(c.get(&1), None);
     }
     {
-        let mut c= LruCache::new(2);
+        let mut c = LruCache::new(2);
         c.put(&1, MockValue::new(1));
         assert_get!(c, 1);
         assert_head!(c, 1);
@@ -147,7 +164,7 @@ fn test_get() {
         assert_head!(c, 2);
     }
     {
-        let mut c= LruCache::new(3);
+        let mut c = LruCache::new(3);
         c.put(&1, MockValue::new(1));
         c.put(&2, MockValue::new(2));
         c.put(&3, MockValue::new(3));
@@ -158,5 +175,31 @@ fn test_get() {
         assert_head!(c, 2);
         c.get(&3);
         assert_head!(c, 3);
+    }
+}
+
+#[test]
+fn test_callback() {
+    {
+        let mut c = LruCache::new(1);
+        let poppable = gen_count(0);
+        let pop_callback = gen_count(0);
+        c.put(&1, MockValue::new_with_pop_count(1, poppable.clone(), pop_callback.clone()));
+        c.put(&2, MockValue::new(2));
+        assert_eq!(poppable.get(), 1);
+        assert_eq!(pop_callback.get(), 1);
+    }
+    {
+        let mut c = LruCache::new(1);
+        let poppable = gen_count(0);
+        let pop_callback = gen_count(0);
+        let mut mock = MockValue::new_with_pop_count(1, poppable.clone(), pop_callback.clone());
+        mock.poppable = false;
+        c.put(&1, mock);
+        let success = c.put(&2, MockValue::new(2));
+        assert!(!success);
+        assert_eq!(poppable.get(), 1);
+        assert_eq!(pop_callback.get(), 0);
+        assert_head!(c, 1);
     }
 }
