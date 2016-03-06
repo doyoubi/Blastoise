a3261d77c0fbef3c87abcda51b0396f655343ed1
doyoubi <doyoubihgx@gmail.com>
2016-03-06 15:46:26 +0000

    finished eval cond

--- src/exec/evaluate.rs
+++ src/exec/evaluate.rs
@@ -4,17 +4,103 @@ use ::parser::condition::{
     ConditionExpr,
     ArithExpr,
     ArithOp,
+    CmpOperantExpr,
+    CmpOp,
+    CondRef,
+    LogicOp,
 };
 use ::parser::common::{ValueExpr, ValueType};
 use ::parser::attribute::AttributeExpr;
 use ::store::buffer::DataPtr;
+use ::store::table::AttrType;
+use ::utils::pointer::read_string;
 
 
-pub type PtrMap = HashMap<(String, String), (DataPtr, ValueType)>;
+pub type PtrMap = HashMap<(String, String), (DataPtr, AttrType)>;
 
 
-pub fn evaluate(_condition : &ConditionExpr, _ptr_map : &PtrMap) -> bool {
-    false
+pub fn eval_cond(condition : &ConditionExpr, ptr_map : &PtrMap) -> bool {
+    match condition {
+        &ConditionExpr::NotExpr{ ref operant } => !eval_cond(operant, ptr_map),
+        &ConditionExpr::CmpExpr{ ref lhs, ref rhs, op } =>
+            eval_cmp_operant(lhs, rhs, op, ptr_map),
+        &ConditionExpr::LogicExpr{ ref lhs, ref rhs, op } =>
+            eval_logic_op(lhs, rhs, op, ptr_map),
+    }
+}
+
+pub fn eval_logic_op(lhs : &CondRef, rhs : &CondRef, op : LogicOp, ptr_map : &PtrMap) -> bool {
+    let lresult = eval_cond(&**lhs, ptr_map);
+    let rresult = eval_cond(&**rhs, ptr_map);
+    match op {
+        LogicOp::Or => lresult || rresult,
+        LogicOp::And => lresult && rresult,
+    }
+}
+
+pub fn eval_cmp_operant(
+        lhs : &CmpOperantExpr,
+        rhs : &CmpOperantExpr,
+        op : CmpOp,
+        ptr_map : &PtrMap) -> bool {
+    match (lhs, rhs) {
+        (&CmpOperantExpr::Value(ref l), &CmpOperantExpr::Value(ref r)) => {
+            let lvalue = eval_str(l);
+            let rvalue = eval_str(r);
+            eval_str_cmp(&lvalue, &rvalue, op)
+        }
+        (&CmpOperantExpr::Value(ref l), &CmpOperantExpr::Arith(ref r)) => {
+            let lvalue = eval_str(l);
+            let rvalue = eval_str_attr(r, ptr_map);
+            eval_str_cmp(&lvalue, &rvalue, op)
+        }
+        (&CmpOperantExpr::Arith(ref l), &CmpOperantExpr::Value(ref r)) => {
+            let lvalue = eval_str_attr(l, ptr_map);
+            let rvalue = eval_str(r);
+            eval_str_cmp(&lvalue, &rvalue, op)
+        }
+        (&CmpOperantExpr::Arith(ref l), &CmpOperantExpr::Arith(ref r)) => {
+            let lvalue = eval_arith(l, ptr_map);
+            let rvalue = eval_arith(r, ptr_map);
+            match op {
+                CmpOp::LT => lvalue < rvalue,
+                CmpOp::GT => lvalue > rvalue,
+                CmpOp::LE => lvalue <= rvalue,
+                CmpOp::GE => lvalue >= rvalue,
+                CmpOp::EQ => lvalue == rvalue,
+                CmpOp::NE => lvalue != rvalue,
+                CmpOp::Is => unimplemented!(),
+                CmpOp::IsNot => unimplemented!(),
+            }
+        }
+    }
+}
+
+pub fn eval_str_cmp(lvalue : &String, rvalue : &String, op : CmpOp) -> bool {
+    match op {
+        CmpOp::LT | CmpOp::GT| CmpOp::LE| CmpOp::GE =>
+            panic!("invalid operationo for string"),
+        CmpOp::EQ => lvalue == rvalue,
+        CmpOp::NE => lvalue != rvalue,
+        CmpOp::Is => unimplemented!(),
+        CmpOp::IsNot => unimplemented!(),
+    }
+}
+
+pub fn eval_str_attr(expr : &ArithExpr, ptr_map : &PtrMap) -> String {
+    match expr {
+        &ArithExpr::Attr( ref attr_expr ) => {
+            let (table, attr) = match attr_expr {
+                &AttributeExpr::TableAttr{ref table, ref attr} => (table.clone(), attr.clone()),
+                &AttributeExpr::AggreFuncCall{ref table, ref attr, ..} => (table.clone(), attr.clone()),
+            };
+            assert!(table.is_some());
+            let (p, t) = ptr_map.get(&(table.unwrap(), attr)).unwrap().clone();
+            let len = extract!(t, AttrType::Char{len}, len);
+            unsafe{ read_string(p, len) }
+        }
+        _ => panic!("expected attribute, found {:?}", expr),
+    }
 }
 
 pub fn eval_arith(expr : &ArithExpr, ptr_map : &PtrMap) -> f32 {
@@ -39,8 +125,8 @@ pub fn eval_arith(expr : &ArithExpr, ptr_map : &PtrMap) -> f32 {
             };
             let (p, t) = ptr_map.get(&(table.unwrap(), attr)).unwrap().clone();
             match t {
-                ValueType::Integer => unsafe{ read::<i32>(p as *const i32) as f32 },
-                ValueType::Float => unsafe{ read::<f32>(p as *const f32) },
+                AttrType::Int => unsafe{ read::<i32>(p as *const i32) as f32 },
+                AttrType::Float => unsafe{ read::<f32>(p as *const f32) },
                 _ => panic!("invalid type {:?}", t),
             }
         }
@@ -51,6 +137,13 @@ pub fn eval_num(expr : &ValueExpr) -> f32 {
     match expr.value_type {
         ValueType::Integer => expr.value.parse::<i32>().unwrap() as f32,
         ValueType::Float => expr.value.parse::<f32>().unwrap(),
+        t => panic!("invalid type {:?}", t),
+    }
+}
+
+pub fn eval_str(expr : &ValueExpr) -> String {
+    match expr.value_type {
+        ValueType::String => expr.value.clone(),
         t => panic!("invalid type {:?}", t),
     }
 }
--- src/test/exec/test_evaluate.rs
+++ src/test/exec/test_evaluate.rs
@@ -1,13 +1,14 @@
 use std::ptr::write;
 use std::ptr::read;
 use libc::malloc;
-use ::parser::condition::ArithExpr;
-use ::parser::common::ValueType;
+use ::parser::condition::{ArithExpr, ConditionExpr};
+use ::store::table::AttrType;
 use ::exec::evaluate::{
     PtrMap,
     eval_arith,
+    eval_cond,
 };
-use ::utils::pointer::pointer_offset;
+use ::utils::pointer::{pointer_offset, write_string};
 
 
 #[test]
@@ -24,9 +25,53 @@ fn test_eval_arith() {
             write::<f32>(float_p as *mut f32, 666.666);
         }
         let mut ptr_map = PtrMap::new();
-        ptr_map.insert(("student".to_string(), "score".to_string()), (int_p, ValueType::Integer));
-        ptr_map.insert(("teacher".to_string(), "score".to_string()), (float_p, ValueType::Float));
+        ptr_map.insert(("student".to_string(), "score".to_string()), (int_p, AttrType::Int));
+        ptr_map.insert(("teacher".to_string(), "score".to_string()), (float_p, AttrType::Float));
         let arith = gen_parse_result!(ArithExpr::parse, "100 + teacher.score + student.score)");
         assert_eq!(eval_arith(&arith, &ptr_map), 999.666);
+    }
+}
+
+#[test]
+fn test_eval_cond() {
+    {
+        let cond = gen_parse_result!(ConditionExpr::parse, "not 2 > 1");
+        assert_eq!(eval_cond(&cond, &PtrMap::new()), false);
+    }
+    {
+        let cond = gen_parse_result!(ConditionExpr::parse, "2 > 1 and 1 == 2 or 3 > 1 and 2 >= 2");
+        assert_eq!(eval_cond(&cond, &PtrMap::new()), true);
+    }
+    {
+        let cond = gen_parse_result!(ConditionExpr::parse, r#" "bb" != "bb" "#);
+        assert_eq!(eval_cond(&cond, &PtrMap::new()), false);
+    }
+    {
+        let int_p = unsafe{ malloc(8) };
+        let float_p = pointer_offset(int_p, 4);
+        unsafe{
+            write::<i32>(int_p as *mut i32, 233);
+            write::<f32>(float_p as *mut f32, 666.666);
+        }
+        let mut ptr_map = PtrMap::new();
+        ptr_map.insert(("student".to_string(), "score".to_string()), (int_p, AttrType::Int));
+        ptr_map.insert(("teacher".to_string(), "score".to_string()), (float_p, AttrType::Float));
+        let cond = gen_parse_result!(ConditionExpr::parse,
+            "student.score = 233 and 666.666 = teacher.score and teacher.score > student.score");
+        assert_eq!(eval_cond(&cond, &ptr_map), true);
+    }
+    {
+        let s = unsafe{ malloc(8) };
+        let f = pointer_offset(s, 4);
+        unsafe{
+            write_string(s, &"aa".to_string(), 4);
+            write::<f32>(f as *mut f32, 666.666);
+        }
+        let mut ptr_map = PtrMap::new();
+        ptr_map.insert(("student".to_string(), "name".to_string()), (s, AttrType::Char{len:4}));
+        ptr_map.insert(("teacher".to_string(), "score".to_string()), (f, AttrType::Float));
+        let cond = gen_parse_result!(ConditionExpr::parse,
+            "student.name = \"aa\" and \"aa\" = student.name and 666.666 = teacher.score");
+        assert_eq!(eval_cond(&cond, &ptr_map), true);
     }
 }
